8934983ea264e9217eef2202f3ba06915d3bedd5
Johnny <tamtingyuen@gmail.com>
2024-03-03 22:56:40 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Johnny Tam on 3/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WeatherView()
+        }
+    }
+}
---
+++ WeatherApp/WeatherView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Johnny Tam on 3/3/2024.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    
+    @State private var isNight = false
+    
+    var body: some View {
+        VStack{
+            Text("Hello World!")
+        }
+    }
+}
+
+#Preview {
+    WeatherView()
+}
